f4918536f09e5904a04eaa57efd84ccae42ff55d
Labssss <91913505+Labssss@users.noreply.github.com>
2022-12-27 12:40:34 +0300

    release

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
-    res.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
+    res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization');
     next();
@@ -23,38 +22,9 @@ app.use(function (req, res, next) {
 app.use("/auth", authRouter)
 app.use("/api", ticketsRouter)
 
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
-// app.get('/', (req, res) => {
-//     merchant_model.getMerchants()
-//     .then(response => {
-//       res.status(200).send(response);
-//     })
-//     .catch(error => {
-//       res.status(500).send(error);
-//     })
-//   })
-  
-// app.post('/merchants', (req, res) => {
-//     merchant_model.createMerchant(req.body)
-//     .then(response => {
-//       res.status(200).send(response);
-//     })
-//     .catch(error => {
-//       res.status(500).send(error);
-//     })
-// })
-  
-// app.delete('/merchants/:id', (req, res) => {
-//     merchant_model.deleteMerchant(req.params.id)
-//     .then(response => {
-//       res.status(200).send(response);
-//     })
-//     .catch(error => {
-//       res.status(500).send(error);
-//     })
-// })
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
